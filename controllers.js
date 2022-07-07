45690e87ec905d841e5ef508e92655ec7de6a98e
YanissB <91958136+YanissB@users.noreply.github.com>
2022-07-07 17:55:55 +0200

    getFurnituresQuantity done

--- controllers.js
+++ controllers.js
@@ -8,14 +8,30 @@ exports.getAllFurnitures = (req, res) => {
 exports.getFurnituresByColor = (req, res) => {
   let colors = req.params.colors;
   const dbFurnitures = db.furnitures;
+  console.log("hello");
   result = [];
-
   for (let i = 0; i < dbFurnitures.length; i++) {
     let dbFurnituresColorsLen = Object.keys(dbFurnitures[i].colors).length;
     for (let j = 0; j < dbFurnituresColorsLen; j++) {
       if (db.furnitures[i].colors[j] === colors) {
         result.push(db.furnitures[i]);
       }
+    }
+  }
+
+  res.send(result);
+};
+
+exports.getFurnituresByQuantity = (req, res) => {
+  let quantity = parseInt(req.params.quantity);
+  const dbFurnitures = db.furnitures;
+  console.log(quantity);
+
+  let result = [];
+
+  for (let i = 0; i < dbFurnitures.length; i++) {
+    if (db.furnitures[i].quantity > quantity) {
+      result.push(db.furnitures[i]);
     }
   }
 
--- router.js
+++ router.js
@@ -6,6 +6,9 @@ const control = require("./controllers.js");
 router.use(cors());
 
 router.get("/furnitures", control.getAllFurnitures);
-router.get("/furnitures/:colors", control.getFurnituresByColor);
+router.get("/furnitures/colors/:colors", control.getFurnituresByColor);
+router.get("/furnitures/quantity/:quantity", control.getFurnituresByQuantity);
+
+// router.patch("/furnitures/:id"), control.patchFurnitureQuantity;
 
 module.exports = router;
